5cdf706ce398f63d1746099b820527ec1fe61441
Matt Davis <mdavis@virtustructure.com>
2020-06-04 11:17:34 -0400

    Updates to fix config not in package

--- privyfilter/Configs/__init__.py
+++ privyfilter/Configs/__init__.py
@@ -1,12 +1,7 @@
 """
 
-privyfilter
+privyfilter configs for face detection
 
 Photo Privacy Filter for GDPR compliant photo sharing and de-identification/bias removal for machine learning.
 
 """
-
-
-
-__version__ = "0.1.12"
-__author__ = "Matthew Davis"
